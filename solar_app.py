b85362db0100ca1e2a185e4a971040f296454b07
protters123 <hekwerken@gmail.com>
2026-04-04 10:40:39 +0200

    Update solar_app.py

--- solar_app.py
+++ solar_app.py
@@ -8,7 +8,7 @@ from datetime import datetime
 # DIT IS DE CORRECTE LINK VOOR EXPORT:
 # DEZE LINK MOET PRECIES ZO ZIJN:
 SHEET_ID = "19wEhTv_-3PkwWl3dnp8xn_e5SKtwBmuJO4yS8W-uEmo"
-CSV_URL = f"https://google.com{SHEET_ID}/export?format=csv"
+CSV_URL = f"https://google.com{19wEhTv_-3PkwWl3dnp8xn_e5SKtwBmuJO4yS8W-uEmo}/export?format=csv"
 
 
 # INVERTERS

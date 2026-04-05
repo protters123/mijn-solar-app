ab0d663c4f92717a9bfbc7bfe1c24c79bb588cac
protters123 <hekwerken@gmail.com>
2026-04-05 13:53:55 +0200

    Update solar_app.py

--- solar_app.py
+++ solar_app.py
@@ -73,7 +73,7 @@ if val_s > st.session_state.p_symo_peak or val_g > st.session_state.p_galvo_peak
 # --- AUTO-ARCHIVEREN LOGICA (13:45) ---
 # --- AUTO-ARCHIVEREN LOGICA (Aangepast naar 13:52) ---
 target_uur = 13
-target_min = 52
+target_min = 54
 
 if nu_lokaal.hour == target_uur and nu_lokaal.minute == target_min:
     vandaag_sleutel = nu_lokaal.strftime('%Y-%m-%d')

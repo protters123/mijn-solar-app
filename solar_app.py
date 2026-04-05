b3d69eee7f8d9de18e7f1d47a458b10f4791f8c3
protters123 <hekwerken@gmail.com>
2026-04-05 14:17:24 +0200

    Update solar_app.py

--- solar_app.py
+++ solar_app.py
@@ -16,7 +16,7 @@ SHEET_ID = "19wEhTv_-3PkwWl3dnp8xn_e5SKtwBmuJO4yS8W-uEmo"
 CSV_URL = f"https://google.com{SHEET_ID}/export?format=csv&gid=0"
 
 # FIX: Jouw Google Script URL (eindigend op /exec)
-WEBAPP_URL = "https://google.com" 
+WEBAPP_URL = "https://script.google.com/macros/s/AKfycbyIBhDGzmQQvokyzBjYT0Nt8qiRFKtElxMCrhelxfPOLNF2NNbAgOP3PAGTSEQEsMmq/exec" 
 
 PUBLIEK_IP = "94.110.235.108" 
 URL_1 = f"http://{PUBLIEK_IP}:8081/api/v1/data"

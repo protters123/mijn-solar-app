a07a2bc74df0817aa92c821a0a7566ebfeafd6a6
protters123 <hekwerken@gmail.com>
2026-04-12 08:17:41 +0200

    Update solar_app.py

--- solar_app.py
+++ solar_app.py
@@ -11,7 +11,7 @@ import pytz
 
 SHEET_ID = "19wEhTv_-3PkwWl3dnp8xn_e5SKtwBmuJO4yS8W-uEmo"
 CSV_URL = f"https://docs.google.com/spreadsheets/d/{SHEET_ID}/export?format=csv&gid=0"
-WEBAPP_URL = "https://script.google.com/macros/s/AKfycbz6-HWiKU_eVlKPeRIiAXTiPsmV0VVprKttU9VCJ18tlXzfiouF_dIp-JewnNVKpDiY/exec"
+WEBAPP_URL = "https://script.google.com/macros/s/AKfycbwbCETLDq-ONG11a0-u2OZxxEiTb83IpgdreGoH-7lvaJMmsMUkj8RHfCp8i54Qewrt/exec"
 
 PUBLIEK_IP = "94.110.235.108"
 URL_1 = f"http://{PUBLIEK_IP}:8081/api/v1/data"
